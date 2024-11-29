330dccc1bb8dce4cfad3a0bf9970173357c96386
aniceleste <anicelester@gmail.com>
2024-11-29 19:22:44 -0300

    barra

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,52 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0; // El índice de la pestaña seleccionada
+
+  // Las pantallas que se mostrarán según el índice seleccionado
+  static List<Widget> _widgetOptions = <Widget>[
+    CarsListScreen(), 
+    AddCarScreen(),  
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index; // Cambiar de pantalla según el índice
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Colección de Autos',
       theme: ThemeData(primarySwatch: Colors.blue),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => CarsListScreen(),
-        '/add': (context) => AddCarScreen(),
-      },
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Colección de Autos'),
+        ),
+        body: _widgetOptions.elementAt(_selectedIndex), 
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex, 
+          onTap: _onItemTapped, 
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Inicio',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: 'Agregar',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
